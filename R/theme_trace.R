ea74d59b8954e5f5b7ca1e633006b694dc8edf91
Hugo Gruson <Bisaloo@users.noreply.github.com>
2024-01-30 18:24:34 +0100

    Add theme_trace() function

---
+++ R/theme_trace.R
@@ -0,0 +1,33 @@
+#' @importFrom ggplot2 %+replace%
+#' @importFrom ggplot2 theme theme_grey element_rect element_text element_line
+theme_custom <- function(...) {
+
+  # https://ggplot2.tidyverse.org/articles/ggplot2-in-packages.html
+
+  custom_grey <- "#505B5B"
+  green_grey <- "#5E7E80"
+
+  theme_grey(...) %+replace%
+    theme()
+
+}
+
+#' A custom \pkg{ggplot2} theme for \pkg{episoap} reports
+#'
+#' @returns
+#' A \pkg{ggplot2} [theme] object
+#'
+#' @examples
+#' library(ggplot2)
+#'
+#' ggplot(mtcars, aes(wt, mpg)) +
+#'   geom_point() +
+#'   labs(title = "Fuel economy declines as weight increases") +
+#'   theme_trace()
+#'
+#' @export
+theme_trace <- function() {
+
+  theme_custom()
+
+}
