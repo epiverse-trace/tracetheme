3dadb4a2a8967088dacf887e37089830c05266ba
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-02-20 12:13:01 +0000

    added vignette_dim and slide_dim data and documentation

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' Epiverse-TRACE default plotting dimensions for vignettes
+#'
+#' @format ## `vignette_dim`
+#' A list with 2 elements:
+#' \describe{
+#'   \item{vig_height}{A single `numeric` for the height of vignette figures.}
+#'   \item{vig_width}{A single `numeric` for the width of vignette figures.}
+#' }
+"vignette_dim"
+
+#' Epiverse-TRACE default plotting dimensions for slides
+#'
+#' @format ## `slide_dim`
+#' A list with 2 elements:
+#' \describe{
+#'   \item{slide_height}{A single `numeric` for the height of slide figures.}
+#'   \item{slide_width}{A single `numeric` for the width of slide figures.}
+#' }
+"slide_dim"
