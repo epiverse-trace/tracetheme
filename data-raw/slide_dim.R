2b7d88bcec03c04fa3ef38d593ccf99e4b3ac4cf
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-02-20 16:18:03 +0000

    update slides dimensions data from testing

--- data-raw/slide_dim.R
+++ data-raw/slide_dim.R
@@ -1,8 +1,8 @@
 ## code to prepare `slide_dim` dataset goes here
 
 slide_dim <- list(
-  slide_height = 9,
-  slide_width = 16
+  slide_height = 3,
+  slide_width = 6
 )
 
 usethis::use_data(slide_dim, overwrite = TRUE)
