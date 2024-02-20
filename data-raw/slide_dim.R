22d3c8533648a7063279d239dc8e0548b8edf97e
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2024-02-20 12:12:36 +0000

    added data-raw files for vignette_dim and slide_dim

---
+++ data-raw/slide_dim.R
@@ -0,0 +1,8 @@
+## code to prepare `slide_dim` dataset goes here
+
+slide_dim <- list(
+  slide_height = 9,
+  slide_width = 16
+)
+
+usethis::use_data(slide_dim, overwrite = TRUE)
---
+++ data-raw/vignette_dim.R
@@ -0,0 +1,8 @@
+## code to prepare `vignette_dim` dataset goes here
+
+vignette_dim <- list(
+  vig_height = 5,
+  vig_width = 8
+)
+
+usethis::use_data(vignette_dim, overwrite = TRUE)
