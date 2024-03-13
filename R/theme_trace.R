1a302552d3d455f1d902ee8a4586a5a7dea2a157
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2024-03-13 12:50:00 +0000

    Update title & caption face, position & size

--- R/theme_trace.R
+++ R/theme_trace.R
@@ -1,12 +1,18 @@
 #' @importFrom ggplot2 %+replace%
-#' @importFrom ggplot2 theme theme_bw element_rect element_text
+#' @importFrom ggplot2 theme theme_bw element_rect element_text rel margin
 theme_custom <- function(...) {
 
   # https://ggplot2.tidyverse.org/articles/ggplot2-in-packages.html
 
 
   theme_bw(...) %+replace%
-    theme(text = element_text(family = "DM Sans"))
+    theme(
+      text = element_text(family = "DM Sans"),
+      plot.title = element_text(
+        hjust = 0.5, face = "bold", size = rel(1.7), margin = margin(b = rel(5))
+      ),
+      plot.caption = element_text(hjust = 1, face = "italic")
+    )
 
 }
 
